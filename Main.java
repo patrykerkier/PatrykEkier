31e0e766fdd1d04b69df16e1656de75b3f320956
patrykerkier <61512007+patrykerkier@users.noreply.github.com>
2020-03-22 16:53:33 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+
+class CechyWspolne
+{
+public int nr;
+public String nazwa;
+public String data_wyd;
+public String autor;
+public String wydawnictwo;
+public int cena;
+
+}
+
+class Ksiazka extends CechyWspolne
+{
+    public int strony;
+    public String gatunek;
+
+}
+
+class Plyta_CD extends CechyWspolne
+{
+    public int pojemnosc;
+
+}
+
+class Ebook extends CechyWspolne
+{
+    public int rozmiar;
+
+}
+
+class Film extends CechyWspolne
+{
+    public int rozmiar;
+    public String gatunek;
+
+}
+
+class Audiobook extends CechyWspolne
+{
+   public int rozmiar;
+
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
